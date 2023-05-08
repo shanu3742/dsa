129d43ac3599ebb5a8c06b6fab530c7bb644d1ed
KUMAR SHANU <shanuK@aarth.io>
2023-05-08 17:47:18 +0530

    shift and unshift method added

--- singlyLinkList.js
+++ singlyLinkList.js
@@ -53,23 +53,57 @@ class LinkList {
     this.size =this.size-1
     return deletedData;
     }
+
+    shift(value){
+        let newNode={
+        value:value,
+        next:this.head
+        }
+
+         this.head=newNode;
+         this.size=this.size+1;
+    }
+
+    unShift(){
+
+     
+        if(!this.head) return;
+        if(this.size===1){
+            this.head=null;
+            this.tail=null;
+            this.size=0;
+            return this.head
+        }
+
+        let currentHead= this.head;
+        this.head= currentHead.next;
+        this.size= this.size-1;
+        return currentHead
+
+
+     
+
+    }
     }
     
     let list = new LinkList('shanu')
-    list.push('kumar')
-    list.push('virat')
-    list.push('ks')
+//     list.push('kumar')
+//     list.push('virat')
+//     list.push('ks')
     
     
-    list.pop()
-    list.pop()
-   console.log( list.pop())
-   list.pop()
+//     list.pop()
+//     list.pop()
+//    console.log( list.pop())
+//    list.pop()
 
-   list.push('kunar_1')
-   list.push('tks_1')
-   list.push('tks_3')
-   list.pop()
-    console.log(list)
+//    list.push('kunar_1')
+//    list.push('tks_1')
+//    list.push('tks_3')
+//    list.pop()
+list.shift('a-1-b')
+list.shift('b-1-c')
+list.unShift()
+console.log(list)
    
     
