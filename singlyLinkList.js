d7482aafff8c377c7ba5ceae9536e2fd6039f4fc
KUMAR SHANU <shanuK@aarth.io>
2023-05-08 16:00:41 +0530

    Add files via upload

--- singlyLinkList.js
+++ singlyLinkList.js
@@ -7,7 +7,8 @@ class LinkList {
             value: value,
             next: null
         }
-        this.tail = this.head
+        this.tail = this.head,
+        this.size=1;
     }
     appendList(value) {
         let newNode = {
@@ -15,11 +16,14 @@ class LinkList {
             next: null
         }
         this.tail.next = newNode;
-        this.tail = newNode
+        this.tail = newNode;
+        this.size= this.size+1
     }
 }
 
 let list = new LinkList('shanu')
 list.appendList('kumar')
 list.appendList('virat')
+
+console.log(list)
 
